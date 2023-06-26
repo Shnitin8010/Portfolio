8b980b7b79f717e0272e7f745be63b0ce7eb650f
Nitin sharma <116473564+Shnitin8010@users.noreply.github.com>
2023-06-26 22:36:55 +0530

    ds

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -72,10 +72,10 @@ export const Home = () => {
                   <img src="/images/me.png" alt="About Me" />
                 </div>
                 <div class="infos">
-                  <h4>A WEB DESIGNER</h4>
-                  <h1>David Henderson.</h1>
-                  <p>I am a Web Designer based in san francisco.</p>
-                  <a href="#" class="about-btn">
+                  <h4>A WEB DESIGNER & SHOPIFY DEV.</h4>
+                  <h1>Nitin Sharma.</h1>
+                  <p>I am a Web Designer & Developer.</p>
+                  <a href="/about" class="about-btn">
                     <img src=" images/icon.svg" alt="Button" />
                   </a>
                 </div>
